dde0baa06bb7654191c825da79e666234add0b40
stephane-vanraes <stephane.vanraes@gmail.com>
2023-12-19 11:00:49 +0100

    a better way to get the draft of a published article

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,24 +1,24 @@
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals, params, url }) => {
-	const posts: App.Post[] = await locals.client.fetch(`
-  *[_type == "post" && slug.current == "${params.slug}"] {
-      _id,
-      author->{
-        bio,
-        image,
-        name,
-        slug
-      },
-      title,
-      publishedAt,
-      body,
-      mainImage,
-      
-    }
-  `);
+	const post: App.Post = (
+		await locals.client.fetch(`
+  *[_type == "post" && slug.current == "${params.slug}"] | order(_updatedAt desc) {    
+    _id,
+    author->{
+      bio,
+      image,
+      name,
+      slug
+    },
+    title,
+    publishedAt,
+    body,
+    mainImage,
+  }
+  `)
+	)[0];
 
-	const post = locals.isPreviewMode ? posts[posts.length - 1] : posts[0];
 	locals.crumbs[2] = { href: url.href, title: post.title };
 	return {
 		post
